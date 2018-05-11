de82f6ec2d0a477f6e2912d31b49fa7b40b4a167
Asbjlørn Gaarde <asbjorn_gaarde@hotmail.com>
2018-05-11 11:59:38 +0200

    added expanation

--- src/Main.hs
+++ src/Main.hs
@@ -22,6 +22,23 @@ runAdder n a = unAdder a n id
 
 megaAdd :: Int -> Int -> Int
 megaAdd n1 n2 = runAdder 0 (addTo n1 >>> addTo n2)
+{-
+Following is a calculation of how the expression (addTo n1 >>> addTo n2)
+gets reduced to something that makes more sense
+
+addTo n1 >>> addTo n2 = Adder \n fn ->
+    let fx x = unAdder (addTo n2) x fn
+    in  unAdder (addTo n1) n fx
+
+unAdder (addTo n2) x fn = (\n' fn' -> fn' (n' + n2)) x fn
+= fn (x + n2)
+fx x = fn (x + n2)
+
+unAdder (addTo n1) n fx = (\n' fn' -> fn' (n' + n1)) n fx
+= fx (n + n1) = fn ((n + n1) + n2)
+
+addTo n1 >>> addTo n2 = Adder $ \n fn -> fn ((n + n1) + n2)
+-}
 
 megaInc :: Int -> Int
 megaInc n = runAdder n inc
